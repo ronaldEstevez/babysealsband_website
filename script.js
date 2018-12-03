9f2166de8d729b932a18741d19d8d76796a2540f
Gregory Strakhov <gstrakho@stevens.edu>
2018-12-03 17:18:31 -0500

    rotate seals

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /*
 To do list for JavaScript additions:
   - ???
-
-
 */
+
+
 
 // function changing text-shadow of title
 var inst = setInterval(changeShadow, 750);
@@ -11,20 +11,38 @@ var inst = setInterval(changeShadow, 750);
 var counter = 0;
 
 function changeShadow() {
-  var shadowIDs = ["orangeShadow", "cyanShadow", "magentaShadow"];
+  var shadowClasses = ["orangeShadow", "cyanShadow", "magentaShadow"];
   var elem = document.getElementById("titular");
 
-  elem.className = shadowIDs[counter];
+  elem.className = shadowClasses[counter];
   counter++;
-  if (counter >= shadowIDs.length) {
+  if (counter >= shadowClasses.length) {
     counter = 0;}
 }
 
+var instR1 = setInterval(rotation1, 750);
+var instR2 = setInterval(rotation2, 750);
 
+var counterR = 1;
 
+function rotation1() {
+  var rotClasses = ["rotate", "rotateLeft", "rotateRight"];
+  var elem = document.getElementById("topRightLogo");
 
+  elem.className = rotClasses[counter];
+  counterR++;
+  if (counterR >= rotClasses.length) {
+    counterR = 1;}
+}
 
+function rotation2() {
+  var rotClasses = ["rotate1", "rotateRight", "rotateLeft"];
+  var elem = document.getElementById("topLeftLogo");
 
-
+  elem.className = rotClasses[counter];
+  counterR++;
+  if (counterR >= rotClasses.length) {
+    counterR = 1;}
+}
 
     // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
